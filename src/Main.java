4ff746c4780c4361c6cb50bf8a56a3845965863d
inarvos@gmail.com <inarvos@gmail.com>
2018-03-04 12:52:33 +0200

    Check three digit's numbers

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,38 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int a = 3, b = 4;
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter a number:");
-		int entered = sc.nextInt();
-		
-		
-		if (entered < a) {
-			System.out.println("The smallest");
-		} else if ((entered >= a) && (entered <= b)) {
-            System.out.println("Between");
+		/*
+		 * Check three digit's number. if it is even then find sum of number's digits else (if odd)
+		 *  find multiply of all number's digits
+		 */
+		int number = 529; //526 -> "526" (int)'5' + (int)'2' + (int)'6'
+	
+				
+		if (number % 2 == 0) {
+			System.out.println(sumOfDigits(number));
 		} else {
-			System.out.println("The largest");
+			System.out.println(productOfDigits(number));
 		}
 		
 		
-		sc.close();
+	}
+
+	private static int productOfDigits(int number) {
+		String stringOfNumber = String.valueOf(number).replace("-", "");
+		int product = 1;
+		for (int i = 0; i < stringOfNumber.length(); i++) {
+			product = product * Integer.valueOf(stringOfNumber.substring(i, i + 1));
+		}	
+		return product;
+	}
+
+	private static int sumOfDigits(int number) {
+		String stringOfNumber = String.valueOf(number).replace("-", "");
+		int sum = 0;
+		for (int i = 0; i < stringOfNumber.length(); i++) {
+			sum = sum + Integer.valueOf(stringOfNumber.substring(i, i + 1));
+		}	
+		return sum;
 	}
 
 }
