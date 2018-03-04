9dac8cc91d337a44919a1c6fd1914897340a57df
inarvos@gmail.com <inarvos@gmail.com>
2018-03-04 12:03:57 +0200

    Given an example of using if-else statements

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,25 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		int a = 3, b = 4;
+		
 		Scanner sc = new Scanner(System.in);
-		System.out.println(
-				"Если хотите сравнить вес на Земле с весом на других планетах СС, введите 1 , с весом на Луне - 2, с весом на Солнце - 3: ");
-		int i = sc.nextInt();
-
-		if ((i == 1) || (i == 2) || (i == 3)) {
-			System.out.print("Вес на Земле(кг.): ");
-			double weightOnTheEarth = sc.nextInt();
-			if (i == 1) {
-				System.out.println("Заданный вес на Плутоне равен: " + weightOnTheEarth * 0.064 + " кг.");
-				System.out.println("Заданный вес на Меркурие равен: " + weightOnTheEarth * 0.378 + " кг.");
-				System.out.println("Заданный вес на Венере равен: " + weightOnTheEarth * 0.905 + " кг.");
-				System.out.println("Заданный вес на Марсе равен: " + weightOnTheEarth * 0.378 + " кг.");
-				System.out.println("Заданный вес на Юпитере равен: " + weightOnTheEarth * 2.302 + " кг.");
-				System.out.println("Заданный вес на Сатурне равен: " + weightOnTheEarth * 0.895 + " кг.");
-				System.out.println("Заданный вес на Уране равен: " + weightOnTheEarth * 0.905 + " кг.");
-				System.out.println("Заданный вес на Нептуне равен: " + weightOnTheEarth * 1.137 + " кг.");
-			}
-			if (i == 2) {
-				System.out.println("Заданный вес на Луне равен: " + weightOnTheEarth * 0.17);
-			}
-			if (i == 3) {
-				System.out.println("Заданный вес на Солнце равен: " + weightOnTheEarth * 27.072);
-			}
+		
+		System.out.println("Enter a number:");
+		int entered = sc.nextInt();
+		
+		
+		if (entered < a) {
+			System.out.println("The smallest");
+		} else if ((entered >= a) && (entered <= b)) {
+            System.out.println("Between");
 		} else {
-			System.out.println("Вы ввели неподходящее число!");
+			System.out.println("The largest");
 		}
 		
+		
 		sc.close();
-
 	}
 
 }
