f4b3203d7b9734f9b0f0208f102478a078f78a74
inarvos@gmail.com <inarvos@gmail.com>
2018-03-24 15:20:02 +0200

    Adding the example of using switch statement...

--- src/ReportCard.java
+++ src/ReportCard.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class ReportCard {
    public static char convertGrades(int grade) {
 	   if (grade > 90) {
@@ -7,12 +7,39 @@ public class ReportCard {
 		   return 'B';
 	   } else if (grade >= 70 && grade < 80) {
 		   return 'C';
-	   } else {
+	   } else if (grade >= 0 && grade < 70) {
 		   return 'D';
+	   } else {
+		   return Character.MIN_VALUE;
 	   }
-   }
+   } 
+   
    public static void main(String[] args) {
 	   System.out.println(ReportCard.convertGrades(88));
 	   System.out.println(ReportCard.convertGrades(55));
+	   Scanner sc = new Scanner(System.in);
+	   int grade = sc.nextInt();
+	   sc.close();
+	   switch (ReportCard.convertGrades(grade)) {
+			case 'A':
+				System.out.println("Excellent job!");
+				break;
+			case 'B':
+				System.out.println("Good job!");
+				break;
+			case 'C':
+				System.out.println("Need to work more!");
+				break;
+			case 'D':
+				System.out.println("Change your attitude!");
+				break;
+			default:
+				System.out.println("We have some problems here...");
+				break;
+
+		}
+	   
+	   //System.out.println((int)Character.valueOf('A'));
    }
+   
 }
