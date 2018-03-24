92c7028ee3a26ee35633060aceb8034433b5bb0a
inarvos@gmail.com <inarvos@gmail.com>
2018-03-24 14:48:03 +0200

    Changing convertGrades method to the static

--- src/ReportCard.java
+++ src/ReportCard.java
@@ -1,19 +1,18 @@
 
 public class ReportCard {
-   public char convertGrades(int grade) {
+   public static char convertGrades(int grade) {
 	   if (grade > 90) {
 		   return 'A';
 	   } else if (grade >= 80 && grade <= 90) {
 		   return 'B';
-	   } else if (grade >= 70 || grade < 80) {
+	   } else if (grade >= 70 && grade < 80) {
 		   return 'C';
 	   } else {
 		   return 'D';
 	   }
    }
    public static void main(String[] args) {
-	   ReportCard repCard = new ReportCard();
-	   System.out.println(repCard.convertGrades(88));
-	   System.out.println(repCard.convertGrades(55));
+	   System.out.println(ReportCard.convertGrades(88));
+	   System.out.println(ReportCard.convertGrades(55));
    }
 }
