3510e506c77cda8827cf97c2534fb71309947cdc
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-04-21 13:47:40 +0300

    Adding Schedule, ScheduleInfo, Group and loading data from a file

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,111 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		//1. Load   schedule from a file
+		
+		List<Schedule.ScheduleInfo> infos = new ArrayList<>();
+		
+		try {
+			
+			List<String> lines = Files.readAllLines(Paths.get("schedule.info"), Charset.forName("CP1251"));
+			
+			
+			for (int i = 1; i < lines.size(); i++) {
+				String[] fields = lines.get(i).split(";");
+				
+				Group g = new Group(Integer.parseInt(fields[0]), Integer.parseInt(fields[1]));
+				
+				Date date = new SimpleDateFormat("dd.MM.yyyy").parse(fields[2]);
+				
+				
+				int dateClass = Integer.parseInt(fields[3]);
+				
+				String subject = fields[4];
+				
+				
+				Schedule.ScheduleInfo si = new Schedule.ScheduleInfo(g, date, dateClass, subject);
+				
+				infos.add(si);
+				
+				
+			}
+			
+			
+			
+		
+		} catch (IOException e) {
+			throw new RuntimeException("File not found " + e.getMessage());
+		}
+		
+		Schedule schedule = new Schedule(infos);
 		
 		//2. Get some input info(group, subgroup and week)
 		
 		//3. Print the schedule for it...
 		
-		Group g = new Group();
 		
-		g.name = "141";
-		g.subgroup = 2;
-		
-		System.out.println();
 		
 		
 	}
 }
 
 class Group {
-	private final String name;
-	private final int subgroup;
+	private final int number;
+	private final int subgroupNumber;
 	
-	public Group(String name, int subgroup) {
+	public Group(int number, int subgroupNumber) {
 		super();
-		this.name = name;
-		this.subgroup = subgroup;
+		this.number = number;
+		this.subgroupNumber = subgroupNumber;
 	}
+
+	@Override
+	public String toString() {
+		return "" + number + "/" + subgroupNumber;
+	}
+}
+
+class Schedule {
+	private List<ScheduleInfo> schedule;
 	
+	public Schedule(List<ScheduleInfo> schedule) {
+		super();
+		this.schedule = schedule;
+	}
+
+	public static class ScheduleInfo {
+		
+		private final Group group;
+		private final Date date;
+		private final int dateClass;
+		private final String subject;
+		
+		public ScheduleInfo(Group group, Date date, int dateClass, String subject) {
+			super();
+			this.group = group;
+			this.date = date;
+			this.dateClass = dateClass;
+			this.subject = subject;
+		}
+
+		@Override
+		public String toString() {
+			return "ScheduleInfo [group=" + group + ", date=" + date + ", dateClass=" + dateClass + ", subject="
+					+ subject + "]";
+		}
+		
+		
+		
+	}
 	
 }
